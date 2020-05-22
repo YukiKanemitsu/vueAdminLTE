9b77212976e16842e8d79ba8645fea4ff26d213c
YukiKanemitsu <y.kanemitsu0723@gmail.com>
2020-05-22 13:15:02 +0900

    "first commit"

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">  
+		<div class="wrapper">
+			<MyHeader></MyHeader>
+			<MyAside></MyAside>
+			<div class="content-wrapper"></div>
+			<router-view/>
+		</div>
+		</div>
+</template>
+
+<script>
+import MyHeader from '@/components/MyHeader'
+import MyAside from '@/components/MyAside'
+
+export default {
+	name: 'app',
+	components: {
+		MyHeader,
+		MyAside
+	}
+}
+</script>
